72e167620e7746d7b9c14320ef6dee35e268cd2b
adsfibonacci <speiglead@gmail.com>
2025-09-16 08:12:42 -0400

    Brand new setup

---
+++ elpa/shell-maker-20250812.1409/shell-maker-pkg.el
@@ -0,0 +1,7 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "shell-maker" "20250812.1409"
+  "Interaction mode for making comint shells."
+  '((emacs "27.1"))
+  :url "https://github.com/xenodium/shell-maker"
+  :commit "d5748939c7209f927a4e563f03ad8a94cf5f802b"
+  :revdesc "d5748939c720")
---
+++ init.el
@@ -0,0 +1,175 @@
+;; Package Initialization
+(require 'package)
+(add-to-list 'package-archives '("elpa" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-devel" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(package-initialize)
+
+(setq debug-on-error t)
+(server-start)
+
+;;(require 'ox-latex)
+(require 'org)
+(with-eval-after-load 'org
+  ;; Ensure org-preview-latex-process-alist is defined
+  (setq org-preview-latex-process-alist
+        '((dvipng :programs ("latex" "dvipng")
+                  :description "dvi > png"
+                  :message "you need to install the programs: latex and dvipng."
+                  :use-xcolor t
+                  :image-input-type "dvi"
+                  :image-output-type "png"
+                  :image-size-adjust (1.0 . 1.0)
+                  :latex-compiler ("latex -interaction nonstopmode -output-directory %o %f")
+                  :image-converter ("dvipng -D %D -T tight -o %O %f")))))
+
+(setq frameset-filter-alist
+      '((background-color . nil)
+        (foreground-color . nil)
+        (cursor-color     . nil)
+        (font             . nil)
+        (tty-type         . nil)
+        (tty              . nil)))
+
+
+;; Enable hs-minor-mode in programming modes
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+(add-hook 'org-mode-hook #'hs-minor-mode)
+
+(require 'yasnippet)
+(yas-global-mode 1)
+(setq org-hide-emphasis-markers t)
+(setq org-confirm-babel-evaluate nil)
+
+(require 'org-modern)
+(add-hook 'org-mode-hook #'org-modern-mode)
+(add-hook 'org-mode-hook #'yas-minor-mode)
+(require 'ob)
+(require 'ob-python)
+(require 'ob-R)
+(require 'ess-site)
+
+(setq org-latex-listings 'minted)
+(setq org-latex-packages-alist '(("" "minted")))
+(setq org-latex-minted-options
+      '(("breaklines" "true")
+        ("fontsize" "\\scriptsize")
+        ("frame" "single")))
+	  
+;; Appearance and functionality
+(load-theme 'atom-one-dark t)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(set-frame-parameter nil 'alpha-background 100)
+(add-to-list 'default-frame-alist '(alpha-background . 100))
+
+;; Disable the built-in C/C++ Flymake backend (flymake-cc)
+(setq flymake-diagnostic-functions
+      (remove 'flymake-cc flymake-diagnostic-functions))
+
+(add-hook 'c-mode-hook
+          (lambda ()
+            (setq-local flymake-diagnostic-functions
+                        (remove 'flymake-cc flymake-diagnostic-functions))))
+
+(add-hook 'c++-mode-hook
+          (lambda ()
+            (setq-local flymake-diagnostic-functions
+                        (remove 'flymake-cc flymake-diagnostic-functions))))
+
+
+;; Set Environment Variables
+(setq inhibit-startup-screen t)
+(setq backup-directory-alist `(("." . ,"~/.emacs.d/.backups")))
+(setq make-backup-files t
+      backup-by-copying t
+      version-control t
+      delete-old-versions t
+      delete-by-moving-to-trash t
+      kept-old-versions 6
+      kept-new-versions 9
+      auto-save-default t
+      auto-save-timeout 20
+      auto-save-interval 200
+      )
+(set-face-foreground 'font-lock-comment-face "#C28aa1")
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(require 'dap-gdb-lldb)
+(require 'dap-cpptools)
+(require 'lsp-mode)
+(add-hook 'c++-mode-hook #'lsp)
+(add-hook 'c-mode-hook #'lsp)
+(add-hook 'lsp-mode-hook #'lsp-lens-mode)
+(require 'rust-mode)
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+(add-hook 'rust-mode-hook #'lsp)
+
+(add-hook 'c++-mode-hook #'hs-minor-mode)
+(add-hook 'c-mode-hook #'hs-minor-mode)
+
+(global-auto-revert-mode t)
+
+(setenv "WORKON_HOME" "~/.pyenv/versions/3.12.7")
+(pyvenv-mode)
+(pyvenv-activate "~/.pyenv/versions/3.12.7")
+
+;; Emacs Usability Hooks
+(add-hook 'emacs-startup-hook (lambda () (interactive) (ansi-term (getenv "SHELL"))))
+
+;; Emacs Keybinds
+(global-set-key (kbd "C-h C-r") 'reload-init)
+(global-set-key (kbd "C-x C-b") 'buffer-menu)
+(global-set-key (kbd "M-s M-s") (lambda () (interactive) (ansi-term (getenv "SHELL"))))
+
+(with-eval-after-load 'treemacs
+  (defun treemacs-custom-filter (file _)
+    (or (s-ends-with? ".aux" file)
+        (s-ends-with? ".log" file)
+	(s-ends-with? ".o" file)))
+  (push #'treemacs-custom-filter treemacs-ignored-file-predicates))
+(desktop-save-mode 1)
+(defun reload-init ()
+  "Reloading Emacs Init"
+  (interactive)
+  (load-file user-init-file))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(display-line-numbers-grow-only t)
+ '(display-line-numbers-widen nil)
+ '(display-line-numbers-width 2)
+ '(elcord-boring-buffers-regexp-list
+   '("^ " "\\\\*Messages\\\\*" "\\\\*ansi-term\\\\*"
+     "\\\\*Customize Group: Elcord\\\\*" "\\\\*Buffer List\\\\*"))
+ '(elcord-display-buffer-details t)
+ '(elcord-display-line-numbers nil)
+ '(elcord-editor-icon nil)
+ '(elcord-idle-message "Researching...")
+ '(elcord-idle-timer 600)
+ '(elcord-quiet t)
+ '(elcord-refresh-rate 10)
+ '(org-babel-load-languages
+   '((emacs-lisp . t) (C . t) (R . t) (python . t) (latex . t)
+     (shell . t)))
+ '(package-selected-packages
+   '(## atom-one-dark-theme auctex dap-mode elcord ess linum-off
+	lsp-latex lsp-ui magit neotree org-modern pdf-tools pyvenv
+	rust-mode treemacs-all-the-icons yasnippet))
+ '(warning-suppress-types '((ox-latex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-string-face ((t nil)))
+ '(line-number ((t (:background "gray8" :foreground "thistle" :slant italic :width ultra-condensed))))
+ '(line-number-current-line ((t (:background "gray20" :foreground "thistle" :slant italic :width ultra-condensed))))
+ '(line-number-major-tick ((t (:background "grey75" :weight bold))))
+ '(org-block ((t (:background "gray5" :foreground "dim gray" :weight semi-bold)))))
+(provide 'init)
