35d92e6440f86f21490fa0fc6edac5876186965b
Alex Speigle <56398573+adsfibonacci@users.noreply.github.com>
2026-01-11 12:49:28 -0500

    Delete elpa directory

--- elpa/shell-maker-20260104.1718/shell-maker-pkg.el
+++
@@ -1,7 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "shell-maker" "20260104.1718"
-  "Interaction mode for making comint shells."
-  '((emacs "27.1"))
-  :url "https://github.com/xenodium/shell-maker"
-  :commit "53742d44b48a13e7550ecf4de824e15963bfa365"
-  :revdesc "53742d44b48a")
