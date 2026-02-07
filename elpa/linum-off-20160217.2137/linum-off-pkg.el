8baf02d8ff43a4eb15119b4c8d5203747656af66
adsfibonacci <speiglead@gmail.com>
2026-02-07 12:58:08 -0500

    Ignore elpa dir

--- elpa/linum-off-20160217.2137/linum-off-pkg.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "linum-off" "20160217.2137"
-  "Provides an interface for turning line-numbering off."
-  ()
-  :url "https://github.com/mattfidler/linum-off"
-  :commit "3e37baaad27d27e405f8dfe01d4ab9cd5b591353"
-  :revdesc "3e37baaad27d"
-  :keywords '("line" "numbering"))
